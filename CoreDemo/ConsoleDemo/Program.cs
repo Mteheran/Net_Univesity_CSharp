9b79c3d6ec149c9f1f78aa8458096378e637c8d7
Miguel teheran <miguelteheran@gmail.com>
2019-06-19 23:07:20 -0500

    Base code
    Base code

---
+++ CoreDemo/ConsoleDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string saludo = "Hola mundo";            
+            var año = 2019;
+            double numeroPi = double.Parse("3,14");
+            Console.WriteLine(saludo);
+            Console.WriteLine(año);
+            Console.WriteLine("Numero pi+1:" + (numeroPi+1));
+        }
+    }
+}
