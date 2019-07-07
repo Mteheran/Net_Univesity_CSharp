f57a5e808056aceb9a7cb82d167d1ab92817e12a
Miguel teheran <miguelteheran@gmail.com>
2019-07-06 20:26:52 -0500

    Update Program.cs
    condicionales

--- CoreDemo/ConsoleDemo/Program.cs
+++ CoreDemo/ConsoleDemo/Program.cs
@@ -6,12 +6,39 @@ namespace ConsoleDemo
     {
         static void Main(string[] args)
         {
-            const string saludo = "Hola mundo";            
-            var año = 2019;
-            double numeroPi = double.Parse("3,14");
-            Console.WriteLine(saludo);
-            Console.WriteLine(año);
-            Console.WriteLine("Numero pi+1:" + (numeroPi+1));
+            Console.WriteLine("Bienvenido a mi aplicacion");
+            Console.WriteLine("1=Saludo, 2=Valor Pi, 3=Año actual");
+            string ValorIngresado = Console.ReadLine();
+
+            //se compara 1 para saludo
+            switch(ValorIngresado)
+        	{
+                case "1":
+                 const string saludo = "Hola mundo";  
+                 Console.WriteLine(saludo);
+                break;
+                case "2":
+                case "3":
+                return;
+                break;
+            }
+
+
+            if (ValorIngresado == "1")
+            {
+                const string saludo = "Hola mundo";  
+                Console.WriteLine(saludo);
+            }
+            else if (ValorIngresado == "2")
+            {
+                 double numeroPi = double.Parse("3,14");  
+                 Console.WriteLine("Numero pi:" + numeroPi.ToString()); 
+            }
+            else if (ValorIngresado == "3")
+            {
+                var año = DateTime.Now.Year;
+                Console.WriteLine(año);
+            }
         }
     }
 }
